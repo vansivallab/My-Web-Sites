7c2f6af51c0fcb5f592de52ab222c285c55535a0
Vansi Vallabhaneni <vansivallab@gmail.com>
2013-12-29 01:45:09 -0600

    updated website, just landing page

--- vansivallab.com/js/main.js
+++ vansivallab.com/js/main.js
@@ -1,57 +1,11 @@
 //main.js
 $(function() {
-var globalView = {
-	$body: $('body'),
-	$navBar: $('#header'),
-	homeView: undefined,
-	projectView: undefined,
-	resumeView: undefined,
-	aboutView: undefined,
-	currView: this.homeView,
-	views: []
-};
-
-function initViews() {
-	globalView.currView = globalView.homeView = new View(globalView, '#', undefined, undefined, '25%');
-	globalView.projectsView = new View(globalView, '#projects', $('#projectsView'), $('#projectsButton'), '0');
-	globalView.resumeView = new View(globalView, '#resume', $('#resumeView'), $('#resumeButton'), '0');
-	globalView.aboutView = new View(globalView, '#about', $('#aboutView'), $('#aboutButton'), '0');
-	globalView.views = [globalView.homeView, globalView.projectsView, globalView.resumeView, globalView.aboutView];
-};
-initViews();
-
-function navigateTo(hash) {
-	if(hash ===  '') {
-		return globalView.homeView.show();
-	}
-	
-	for(var v = 0; v < globalView.views.length; v++) {
-		var view = globalView.views[v];
-		if(view.hash === hash) {
-			return view.show();
-		}
-	}
-}
-
-if (window.location.hash === '') {
-	window.location.hash = globalView.homeView.hash;
-} 
-else {
-	navigateTo(window.location.hash);
-}
-
-window.onhashchange = function() {
-	// actually perform the navigation
-	navigateTo(window.location.hash);
-};
-
-$('.navLink').on('click', function(e) {
-	e.preventDefault();
-	var hash = $(this).attr('href');
-	//console.log("link clicked "+hash);
-	navigateTo(hash);
-});
-
+  var pd = new ProjectData(new Date(2009, 8, 1), new Date(), "timeline");
+  pd.addProject(new Date(2006, 8, 1), new Date(2010, 6, 1), "Westwood Robotics", "robots robots robots");
+  pd.addProject(new Date(2011, 6, 1), new Date(2011, 8, 1), "Human Centered Research Lab", "spinnie");
+  pd.addProject(new Date(2012, 6, 1), new Date(2012, 8, 1), "The Advisory Board Company", "web app");
+  pd.addProject(new Date(2013, 1, 1), new Date(2013, 6, 1), "NavPal", "indoor mapping");
+  pd.addProject(new Date(2013, 6, 1), new Date(2013, 8, 1), "Amazon", "web services");
 });
 
 
@@ -60,4 +14,10 @@ var util = {};
 util.exists = function(obj) {
 	return obj !== null && obj !== undefined;
 };
+
+function InvalidParameterException(message) {
+  this.name = "InvalidParameter";
+  this.message = message;
+}
+
 
